40bf7864eb9c2302b4472e56e5ee7f0090741d04
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-23 19:38:18 -0400

    Replace console.log to 'return' keyword

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -21,9 +21,9 @@ const assertEqual = function (actual, expected) {
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
   }
   else {
-    console.log(`🛑🛑🛑  Assertion Failed: ${arr1} !== ${arr2}`);
+    return `🛑🛑🛑  Assertion Failed: ${arr1} !== ${arr2}`;
   }
 };
