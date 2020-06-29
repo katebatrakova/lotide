51364994e423325ef7ca8b64d8c57941502f0d09
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 18:51:28 -0400

    Convert tailTest to use Mocha and Chai

--- tailTest.js
+++ tailTest.js
@@ -1,9 +1,23 @@
+const assert = require('chai').assert;
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual');
 
 
-const result = ["Hello", "Lighthouse", "Labs"];
+describe("#Tail Testing", () => {
+  it("returns length 2 without the head", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("returns the first element 'Lighthouse", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], 'Lighthouse');
+  });
+  it("returns the second element 'Labs", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], 'Labs');
+  });
+  it("returns correct tail of the array without the value at index 0", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns correct tail of the numbers array without the value at index 0", () => {
+    assert.deepEqual(tail([20, 30, 40, 50, 60]), [30, 40, 50, 60]);
+  });
 
-console.log(assertEqual(tail(result).length, 2)); // ensure we get back two elements
-console.log(assertEqual(tail(result)[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(tail(result)[1], "Labs")); // ensure second element is "Labs"
+
+});
