6abfcd2d76694277110f37bce0fea98bed9d23cc
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 19:59:59 -0400

    Create and export the object referencing all Lotide library functions

---
+++ index.js
@@ -0,0 +1,29 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+
+module.exports = {
+  head: head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  findKeyByValue,
+  findKey,
+  eqObjects,
+  eqArrays,
+  countOnly,
+  countLetters
+};
