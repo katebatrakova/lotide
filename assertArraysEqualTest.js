eebf49a96deff6351a513a8dcbb3699958e1836f
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 14:36:21 -0400

    Add assertArraysEqualTest

---
+++ assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+
+console.log(assertArraysEqual([1, 4, 3], [1, 4, 3]))
+console.log(assertArraysEqual(['red', 'orange', 7], ['red', 'orange', 7]))
