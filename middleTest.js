5e234afe379f3acd61676baf51e6138a61383650
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 18:52:04 -0400

    Convert middleTest to use Mocha and Chai

--- middleTest.js
+++ middleTest.js
@@ -1,8 +1,19 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+
+
+describe("#Middle array Testing", () => {
+  it("should return [7,8]  for [5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([5, 6, 7, 8, 9, 10]), [7, 8]);
+  });
+  it("should return  [7] for [5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(middle([5, 6, 7, 8, 9]), [7]);
+  });
+
+  it("should return  ['d'] for ['a','b','c','d','e','f','g']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c', 'd', 'e', 'f', 'g']), ['d']);
+  });
+});
 
 
 
-
-console.log(assertArraysEqual(middle([5, 6, 7, 8, 9, 10]), [7, 9]));
-console.log(assertArraysEqual(middle([5, 6, 7, 8, 9, 10]), [7, 8]));
