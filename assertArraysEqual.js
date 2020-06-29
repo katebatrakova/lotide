c1115aa9e87abad6b78d842f0bd3e8a95bdfac4e
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 14:35:24 -0400

    Update the file to use module.exports and require functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,6 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
+const assertEqual = require('./assertEqual')
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -27,3 +10,5 @@ const assertArraysEqual = function (arr1, arr2) {
     return `🛑🛑🛑  Assertion Failed: ${arr1} !== ${arr2}`;
   }
 };
+
+module.exports = assertArraysEqual;
