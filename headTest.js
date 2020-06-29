13d0c5aaaaa4116ffcdf878abb5ea3e88f709712
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 18:50:22 -0400

    Convert headTest.js use Mocha and Chai

--- headTest.js
+++ headTest.js
@@ -1,5 +1,12 @@
-const head = require('../head.js');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head = require('../head');
 
+describe("#head Testing", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-console.log(assertEqual(head([4, 5, 6, 7, 8]), 4))
+});
