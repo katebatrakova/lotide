41652f665cb3d2cb55b841e9d18668600123333e
Kateryna Batrakova <63267743+katebatrakova@users.noreply.github.com>
2020-06-29 14:47:14 -0400

    Update file to use module.exports

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
-  }
-  else {
-    return `🛑🛑🛑  Assertion Failed: ${arr1} !== ${arr2}`;
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function (arr) {
   if (arr.length === 1 || arr.length === 2 || arr.length === 0) {
@@ -32,4 +13,4 @@ const middle = function (arr) {
     return [arr[index - 1], arr[index]];
   }
 }
-console.log(assertArraysEqual(middle([5, 6, 7, 8, 9, 10]), [7, 9]));
+module.exports = middle;
